e6d8b6ef815eac72171ec08085f0609199462a38
RyanColton <ryan.m.colton@gmail.com>
2017-01-18 12:06:06 -0700

    Finished Project

---
+++ service.js
@@ -0,0 +1,10 @@
+angular.module('myApp').service('mainService', function($http, $q){
+  var BASE_URL = 'http://pokeapi.co/api/v2/';
+
+  this.getPokemon = function(){
+    return $http({
+      method: 'GET',
+      url: BASE_URL + 'pokemon'
+    })
+  }
+})
